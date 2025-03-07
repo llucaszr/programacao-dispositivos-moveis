a8057b0d5c51918bfe8752a142af4fab6c903794
Lucas <llcsz.rocha@gmail.com>
2025-03-06 21:55:21 -0300

    Adicionando arquivos da aula 03

---
+++ aulas/aula03/App.js
@@ -0,0 +1,27 @@
+import { View } from "react-native";
+
+function App() {
+  return (
+    <View style={{ flex: 1, flexDirection: "row", backgroundColor: "white" }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-evenly",
+          alignItems: "stretch",
+          backgroundColor: "red",
+        }}
+      >
+        <View
+          style={{ height: 50, backgroundColor: "black" }}
+        ></View>
+        <View style={{ height: 50, backgroundColor: "blue" }}></View>
+        <View
+          style={{ height: 50, backgroundColor: "yellow" }}
+        ></View>
+      </View>
+      <View style={{ flex: 1, backgroundColor: "green" }}></View>
+    </View>
+  );
+}
+
+export default App;
