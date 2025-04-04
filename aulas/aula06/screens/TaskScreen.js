3191ba10c933d5b221954e8e3a81ed58253dc523
Lucas <llcsz.rocha@gmail.com>
2025-04-03 21:54:36 -0300

    Adicionando os arquivos da aula06.

--- aulas/aula06/screens/TaskScreen.js
+++ aulas/aula06/screens/TaskScreen.js
@@ -1,26 +1,85 @@
 import { useState } from "react";
-import { View, StyleSheet } from "react-native";
-import { Appbar, List, TextInput, FAB, Modal, Button } from "react-native-paper";
+import { View, StyleSheet, FlatList } from "react-native";
+import {
+  Appbar,
+  List,
+  TextInput,
+  FAB,
+  Modal,
+  Button,
+  Divider,
+  Text
+} from "react-native-paper";
 
 function TaskScreen() {
-  const [concluida, setConcluida] = useState(false);
+  const [tarefas, setTarefas] = useState([]);
+  const [tarefa, setTarefa] = useState("");
+  const [refresh, setRefresh] = useState(false);
   const [exibeModal, setExibeModal] = useState(false);
+  const [exibeAlerta, setExibeAlerta] = useState(false);
 
   return (
     <View style={styles.container}>
       <Appbar.Header>
         <Appbar.Content title="Minhas Tarefas" />
       </Appbar.Header>
-      <List.Item
-        onPress={() => setConcluida(!concluida)}
-        title="Estudar para a prova"
-        right={(props) => <List.Icon icon= {concluida ? "check-circle-outline" : "circle-outline"} />}
+      <FlatList
+        data={tarefas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <>
+            {refresh && <></>}
+            <List.Item
+              onLongPress={() => {
+                setExibeAlerta(true)
+              }}
+              onPress={() => {
+                item.concluida = !item.concluida;
+                setRefresh(!refresh);
+              }}
+              title={item.nome}
+              right={(props) => (
+                <List.Icon
+                  {...props}
+                  icon={
+                    item.concluida ? "check-circle-outline" : "circle-outline"
+                  }
+                />
+              )}
+            />
+            <Divider />
+          </>
+        )}
       />
-      <FAB onPress={() => setExibeModal(true)}
-      icon="plus" style={styles.fab} />
+      <FAB onPress={() => setExibeModal(true)} icon="plus" style={styles.fab} />
       <Modal visible={exibeModal}>
-        <TextInput label="Nova Tarefa" />
-        <Button onPress={() => setExibeModal(false)}>Salvar</Button>
+        <View style={styles.modal}>
+          <TextInput
+            label="Nova Tarefa"
+            onChangeText={(text) => setTarefa(text)}
+          />
+          <Button
+            onPress={() => {
+              if (tarefa) {
+                setTarefas([
+                  ...tarefas,
+                  { id: tarefas.length + 1, nome: tarefa, concluida: false },
+                ]);
+              }
+              setTarefa("");
+              setExibeModal(false);
+            }}
+          >
+            Salvar
+          </Button>
+        </View>
+      </Modal>
+      <Modal visible={exibeAlerta}>
+        <View style={styles.modal}>
+          <Text variant="labelLarge">Deseja apagar a tarefa?</Text>
+          <Button onPress={() => setExibeAlerta(false)}>Não</Button>
+          <Button onPress={() => setExibeAlerta(false)}>Sim</Button>
+        </View>
       </Modal>
     </View>
   );
@@ -34,6 +93,12 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 16,
     bottom: 16,
+  },
+  modal: {
+    backgroundColor: "white",
+    padding: 16,
+    margin: 16,
+    borderRadius: 8,
   },
 });
 
